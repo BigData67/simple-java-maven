5dda3756fa600f50e9b86d5167585a83c13ca3dc
bigdata67 <bigdata167@gmail.com>
2018-01-27 23:24:57 +0100

    Commit 2

--- src/main/java/com/formation/app/App.java
+++ src/main/java/com/formation/app/App.java
@@ -6,7 +6,7 @@ package com.formation.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World! Martin";
 
     public App() {}
 
